1ccd1665d23c8f13130c36dff05f3ed6fb7e1b41
AB <absule032@gmail.com>
2025-12-29 10:57:44 +0100

    more additions

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 ###### This project showcases my previous works. It gives potential clients an idea of my skillset and serves as a calling card. AI was used in its creation.
+
+Project notes:
+
+1. missing light/dark mode mode alternation function
+2. Will add a visits and unique visitors count (already tracking those metrics).
+3. Image container in about section may need Responsive anim effects.
+4. Will Add a quick contact button with preset message templates.
+5. Add light mode colour variants.
--- lib/test_cookies.ts
+++ lib/test_cookies.ts
@@ -1,14 +1,17 @@
 "use client";
 
 type cookie = {
-  expires?: Date
-  path:string;
-  domain:string;
-  sameSite: "lax"|"strict"|"none"
-}
+  name: string;
+  expires?: Date;
+  path: string;
+  domain: string;
+  sameSite: "lax" | "strict" | "none";
+};
 
-export default function setCookie(cookie:cookie) {
-  document. 
+export default function setCookie(cookie: cookie) {
+  const { name, expires: e, path, domain, sameSite } = cookie;
+  const expires = e || new Date(Date.now() + 1000 * 3600 * 24 * 28);
+  document.cookie = `name=${name}; expires=`;
 }
 
 document.cookie = "name=session; expires=Fri, 31 Dec 2025 23:59:59 GMT; path=/";
